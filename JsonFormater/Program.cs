21b142da879b2adc3af90feae25ac1eec2970ae6
Maksim Lohinau <Maksim.Lohinau@ventionteams.com>
2024-12-02 12:24:16 +0200

    Add project files.

---
+++ JsonFormater/Program.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string json = @"
+        {
+           ""type"":""Container"",
+           ""items"":[
+              {
+                 ""type"":""TextBlock"",
+                 ""text"":""text""
+              },
+              {
+                 ""type"":""Container"",
+                 ""items"":[
+                    {
+                       ""type"":""TextBlock"",
+                       ""text"":""${tex1}""
+                    },
+                    {
+                       ""type"":""Container"",
+                       ""items"":[
+                          {
+                             ""type"":""TextBlock"",
+                             ""text"":""${text2}""
+                          }
+                       ]
+                    }
+                 ]
+              },
+              {
+                 ""type"":""Container"",
+                 ""specific"":""true"",
+                 ""items"":[
+                    {
+                       ""type"":""TextBlock"",
+                       ""text"":""${text3}""
+                    },
+                    {
+                       ""type"":""Container"",
+                       ""items"":[
+                          {
+                             ""type"":""TextBlock"",
+                             ""text"":""${text4}""
+                          }
+                       ]
+                    }
+                 ]
+              }
+           ]
+        }";
+
+        var jsonObject = JObject.Parse(json);
+        ProcessContainer(jsonObject);
+        Console.WriteLine(jsonObject.ToString());
+    }
+
+    static void ProcessContainer(JObject container)
+    {
+        bool isSpecific = container.ContainsKey("specific") && container["specific"]?.ToString() == "true";
+
+        if (container.ContainsKey("items") && container["items"] is JArray items)
+        {
+            foreach (var item in items)
+            {
+                if (item is JObject itemObject)
+                {
+                    if (itemObject["type"]?.ToString() == "TextBlock" && isSpecific)
+                    {
+                        var text = itemObject["text"]?.ToString();
+                        if (!string.IsNullOrEmpty(text) && text.Contains("${"))
+                        {
+                            itemObject["text"] = "REPLACED VALUE";
+                        }
+                    }
+                    else if (itemObject["type"]?.ToString() == "Container")
+                    {
+                        ProcessContainer(itemObject);
+                    }
+                }
+            }
+        }
+    }
+}
